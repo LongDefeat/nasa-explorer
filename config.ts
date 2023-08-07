ce4b569d88afb592127ff5c20edd2d1e154798ee
LongDefeat <masonsams7@gmail.com>
2023-08-06 21:30:12 -0700

    first commit

---
+++ config.ts
@@ -0,0 +1,5 @@
+const nasaConfig = {
+    apiKey: "nUXlOwgXoVXZIvuZi5SkL8iL6umjgMEpfVNwfDAR"
+}
+
+export default nasaConfig;
---
+++ pages/index.tsx
@@ -0,0 +1,41 @@
+import { NextPage } from 'next';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+import nasaConfig from '../config';
+import styles from './Home.module.css';
+
+const HomePage: NextPage = () => {
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const res = await axios.get('https://api.nasa.gov/planetary/apod', {
+          params: {
+            api_key: nasaConfig.apiKey
+          }
+        });
+        setData(res.data);
+      } catch (e) {
+        console.error(e);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  return (
+    <div className={styles.container}>
+      <h1 className={styles.intro}>Welcome to the NASA Explorer</h1>
+      {data && (
+        <div>
+          <h2 className={styles.title}>{data.title}</h2>
+          <img src={data.url} alt={data.title} className={styles.photo}/>
+          <p className={styles.explanation}>{data.explanation}</p>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default HomePage;
