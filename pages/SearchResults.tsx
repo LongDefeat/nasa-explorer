1453f5172dbf3b534072fd0dee9bcb6fc064dcf5
LongDefeat <masonsams7@gmail.com>
2023-08-08 22:31:33 -0700

    added home button and styling

--- pages/SearchResults.tsx
+++ pages/SearchResults.tsx
@@ -1,5 +1,6 @@
 import { NextPage } from 'next';
 import { useRouter } from 'next/router';
+import Link from 'next/link';  // Import the Link component
 import axios from 'axios';
 import { useEffect, useState } from 'react';
 import nasaConfig from '../config';
@@ -23,11 +24,8 @@ const SearchResults: NextPage = () => {
             page: currentPage
           }
         });
-        
-        // Navigate the response correctly to get the images
-        setData(res.data.collection.items.slice(0, 10)); 
 
-        console.log(res.data.collection.items); 
+        setData(res.data.collection.items.slice(0, 10)); 
       } catch (e) {
         console.error(e);
       }
@@ -35,26 +33,34 @@ const SearchResults: NextPage = () => {
 
     fetchPhotos();
   }, [router.query, currentPage]);
+
   useEffect(() => {
-    // Scolls to top of page whenever data changes
     window.scrollTo(0, 0);
   }, [data]);
 
   return (
-    <div className={styles.container}>
-      {data.map((photo, index) => (
-        <div key={index} className={styles.resultCard}>
-          <img src={photo.links[0].href} alt={photo.data[0].title} className={styles.photo} />
-          <h2 className={styles.resultTitle}>{photo.data[0].title}</h2>
-        </div>
-      ))}
-      <div className={styles.pagination}>
-        {currentPage > 1 && (
-          <button onClick={() => setCurrentPage(currentPage - 1)} className={styles.prevPage}>Previous</button>
-        )}
-          <button onClick={() => setCurrentPage(currentPage + 1)} className={styles.nextPage}>Next</button>
+    <div className={styles.pageWrapper}>
+      <div className={styles.homeButtonContainer}>
+        <Link href="/">  {/* Use Link to redirect to home */}
+            <button className={styles.homeButton}>Home</button>
+        </Link>
       </div>
-    </div>
+        
+        <div className={styles.container}>
+          {data.map((photo, index) => (
+            <div key={index} className={styles.resultCard}>
+              <img src={photo.links[0].href} alt={photo.data[0].title} className={styles.photo} />
+              <h2 className={styles.resultTitle}>{photo.data[0].title}</h2>
+            </div>
+          ))}
+          <div className={styles.pagination}>
+            {currentPage > 1 && (
+              <button onClick={() => setCurrentPage(currentPage - 1)} className={styles.prevPage}>Previous</button>
+            )}
+              <button onClick={() => setCurrentPage(currentPage + 1)} className={styles.nextPage}>Next</button>
+          </div>
+        </div>
+      </div>
   );
 };
 
