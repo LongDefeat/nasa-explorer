3f745a15e199c281f399a6d11344830163af4081
LongDefeat <masonsams7@gmail.com>
2023-08-09 20:49:53 -0700

    add mars rover sliding gallery

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,6 +3,7 @@ import { NextPage } from 'next';
 import { useRouter } from 'next/router';
 import axios from 'axios';
 import React, { useEffect, useState } from 'react';
+import Slider from "react-slick";
 
 // Internal Component and Config Imports
 import nasaConfig from '../config';
@@ -10,11 +11,36 @@ import StarryBackground from '../components/StarryBackground';
 
 // Styles Import
 import styles from './Home.module.css';
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 const HomePage: NextPage = () => {
   const [data, setData] = useState(null);
   const [query, setQuery] = useState('');
+  const [roverImages, setRoverImages] = useState([])
   const router = useRouter();
+  const fetchRoverImages = async () => {
+    try {
+      const res = await axios.get('https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos', {
+        params: {
+          sol: 1000, // get photos from 1000th Martian day since rover arrival
+          api_key: nasaConfig.apiKey
+        } 
+      });
+      setRoverImages(res.data.photos.slice(0, 10)); 
+    } catch (err){
+      console.error(err)
+    }
+  }
+
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    autoplay: true
+  };
 
   // Fetch NASA Photo of the Day on mount
   useEffect(() => {
@@ -25,6 +51,7 @@ const HomePage: NextPage = () => {
             api_key: nasaConfig.apiKey
           }
         });
+        console.log(res.data)
         setData(res.data);
       } catch (e) {
         console.error(e);
@@ -32,6 +59,24 @@ const HomePage: NextPage = () => {
     };
 
     fetchData();
+  }, []);
+
+  useEffect(() => {
+    const fetchRoverImages = async () => {
+      try {
+        const res = await axios.get(`https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos`, {
+          params: {
+            sol: 1000,  // Example: get photos from the 1000th Martian day since landing
+            api_key: nasaConfig.apiKey
+          }
+        });
+        setRoverImages(res.data.photos.slice(0, 10)); // Taking only first 10 for display
+      } catch (e) {
+        console.error(e);
+      }
+    };
+  
+    fetchRoverImages();
   }, []);
 
   const handleSearch = async (event: React.FormEvent) => {
@@ -62,6 +107,20 @@ const HomePage: NextPage = () => {
           </div>
         )}
       </div>
+    <section className={styles.galleries}>
+    <h2 className={styles.marsTitle}>What's Happening on Mars?</h2>
+    <div className={styles.slideshowContainer}>
+    <Slider {...settings}>
+      {roverImages.map((photo) => (
+        <div key={photo.id}>
+          <img src={photo.img_src} alt={`Mars Rover - ${photo.id}`} className={styles.roverImage} />
+        </div>
+      ))}
+    </Slider>
+</div>
+
+    </section>
+
     </>
   );
 };
